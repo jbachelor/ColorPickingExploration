07bb708d2b2ed3b0f784faf0552397ee94c52bd5
Jonathan Bachelor <jon_bachelor@me.com>
2017-06-01 11:24:34 -0700

    Fixing errors from merge.

--- ColorPicker1/ColorPicker1/ViewModels/SkiaPicker1PageViewModel.cs
+++ ColorPicker1/ColorPicker1/ViewModels/SkiaPicker1PageViewModel.cs
@@ -14,22 +14,23 @@ namespace ColorPicker1.ViewModels
 	{
 		public DelegateCommand<SimplePoint> CanvasTappedCommand { get; set; }
 
+		public SkiaPicker1PageViewModel()
+		{
 			CanvasTappedCommand = new DelegateCommand<SimplePoint>(OnCanvasTappedAsync);
-
 		}
 
 		private async void OnCanvasTappedAsync(SimplePoint pointTapped)
 		{
 			Debug.WriteLine($"**** {this.GetType().Name}.{nameof(OnCanvasTappedAsync)}");
 
-            foreach (var kvp in Globals.colorDictionary)
-            {
-                if (kvp.Key.Contains((float)pointTapped.X, (float)pointTapped.Y))
-                {
-                    Globals.ColorChosen = kvp.Value;
-                    break;
-                }
-            }   
-        }
-    }
+			foreach (var kvp in Globals.colorDictionary)
+			{
+				if (kvp.Key.Contains((float)pointTapped.RawX, (float)pointTapped.RawY))
+				{
+					Globals.ColorChosen = kvp.Value;
+					break;
+				}
+			}
+		}
+	}
 }
